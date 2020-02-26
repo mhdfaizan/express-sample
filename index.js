de9f8f7abcaecb85a7387e4a5843a41a57446c0d
mhdfaizan <mohammad.faizan@inseyab.com>
2020-02-26 21:57:35 +0500

    jwt token verification added to sample data api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 var app = express();
 const bodyparser = require('body-parser');
 
@@ -33,10 +34,42 @@ mysqlConnection.connect(
 app.listen(port, () => console.log('Express server is running at port ' + port));
 
 
+// format of token
+// Authorization: Bearer <access_token>
+
+// function to verify token
+const verifyToken = (req, res, next) => {
+    // get auth header value
+    const bearerHeader = req.headers['authorization'];
+    //check if bearer is undefined
+    if (typeof bearerHeader !== 'undefined') {
+        //split at the space
+        const bearer = bearerHeader.split(' ');
+        // get token from array
+        const bearerToken = bearer[1];
+        // set the token
+        req.token = bearerToken;
+        // next middleware
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+};
+
 
 // get sample data from mock file
-app.get('/sample', (req, res) => {
-    res.send(sampledata);
+app.get('/sample', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretKey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.send({
+                sampledata,
+                authData
+            });
+
+        }
+    });
 });
 
 // get all countries data
@@ -51,12 +84,29 @@ app.get('/countries', (req, res) => {
 });
 
 // get single country data
-app.get('/countries/:id', (req, res) => {
-    mysqlConnection.query('SELECT * FROM country WHERE Code = ?', [req.params.id], (err, rows, fields) => {
+app.get('/countries/:code', (req, res) => {
+    mysqlConnection.query('SELECT * FROM country WHERE Code = ?', [req.params.code], (err, rows, fields) => {
         if (!err) {
             res.send(rows);
         } else {
             console.log("Error: " + JSON.stringify(err, undefined, 2));
         }
     })
+});
+
+// login api for generating token
+app.post('/login', (req, res) => {
+    //creating a mock user
+    const user = {
+        id: 1,
+        username: 'brad',
+        email: 'brad@gmail.com'
+    }
+    jwt.sign({
+        user: user
+    }, 'secretKey', (err, token) => {
+        res.send({
+            token: token
+        });
+    });
 });
